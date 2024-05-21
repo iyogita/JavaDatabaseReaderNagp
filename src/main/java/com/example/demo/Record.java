13effd3eb1fca96c7af007c2f9ae2b61a04e35ce
Yogita Singh <yogita.singh@nagarro.com>
2024-05-22 03:18:10 +0530

    fixed pom

--- src/main/java/com/example/demo/Record.java
+++ src/main/java/com/example/demo/Record.java
@@ -1,4 +1,4 @@
-package com.example.entity;
+package com.example.demo;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
--- src/main/java/com/example/demo/RecordRepository.java
+++ src/main/java/com/example/demo/RecordRepository.java
@@ -1,4 +1,4 @@
-package com.example.respository;
+package com.example.demo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
