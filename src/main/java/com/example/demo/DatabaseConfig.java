a87eac03303f74d9add1c0c567ecf8df37a4736b
Yogita Singh <yogita.singh@nagarro.com>
2024-05-24 19:32:18 +0530

    Test

--- src/main/java/com/example/demo/DatabaseConfig.java
+++ src/main/java/com/example/demo/DatabaseConfig.java
@@ -2,7 +2,6 @@ package com.example.demo;
 
 import javax.sql.DataSource;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -11,22 +10,13 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 @Configuration
 public class DatabaseConfig {
 
-    @Value("${spring.datasource.url}")
-    private String url;
-
-    @Value("${spring.datasource.username}")
-    private String username;
-
-    @Value("${spring.datasource.password}")
-    private String password;
-
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        dataSource.setUrl(url);
-        dataSource.setUsername(username);
-        dataSource.setPassword(password);
+        dataSource.setUrl("jdbc:mysql://mysql-0.mysql:3306/mysql");
+        dataSource.setUsername("root");
+        dataSource.setPassword("yourpassword");
         return dataSource;
     }
 
