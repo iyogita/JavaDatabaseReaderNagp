ca7fdcc460524860130ac4f0ae426a2913113bfd
Yogita Singh <yogita.singh@nagarro.com>
2024-05-24 20:37:30 +0530

    po

--- src/main/java/com/example/demo/RecordRepository.java
+++ src/main/java/com/example/demo/RecordRepository.java
@@ -2,7 +2,9 @@ package com.example.demo;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RecordRepository extends JpaRepository<Record, Long> {
     // Custom query methods can be defined here
 }
