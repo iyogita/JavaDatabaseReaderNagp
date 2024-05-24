6ea85dabb3f6151b061f67d209e7e805ce3eda68
Yogita Singh <yogita.singh@nagarro.com>
2024-05-24 20:25:39 +0530

    po

--- src/main/java/com/example/demo/DatabaseConfig.java
+++ src/main/java/com/example/demo/DatabaseConfig.java
@@ -1,30 +1,30 @@
-package com.example.demo;
+// package com.example.demo;
 
-import javax.sql.DataSource;
+// import javax.sql.DataSource;
 
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.JdbcTemplate;
+// import org.springframework.boot.jdbc.DataSourceBuilder;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.jdbc.core.JdbcTemplate;
 
-@Configuration
-public class DatabaseConfig {
+// @Configuration
+// public class DatabaseConfig {
 
-    @Bean
-    public DataSource dataSource() {
-         return DataSourceBuilder.create()
-          .driverClassName("com.mysql.cj.jdbc.Driver")
-          .url("jdbc:mysql://mysql-0.mysql:3306/mysql")
-          .username("root")
-          .password("yourpassword")
-          .build();
-    }
+//     @Bean
+//     public DataSource dataSource() {
+//          return DataSourceBuilder.create()
+//           .driverClassName("com.mysql.cj.jdbc.Driver")
+//           .url("jdbc:mysql://mysql-0.mysql:3306/mysql")
+//           .username("root")
+//           .password("yourpassword")
+//           .build();
+//     }
 
-    @Bean
-    public JdbcTemplate jdbcTemplate() {
-        return new JdbcTemplate(dataSource());
-    }
-}
+//     @Bean
+//     public JdbcTemplate jdbcTemplate() {
+//         return new JdbcTemplate(dataSource());
+//     }
+// }
 
 
 
