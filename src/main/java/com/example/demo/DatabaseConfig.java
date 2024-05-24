536f60fc2288d672cb3fe296b82aec346a56c2dc
Yogita Singh <yogita.singh@nagarro.com>
2024-05-24 20:10:11 +0530

    yo

--- src/main/java/com/example/demo/DatabaseConfig.java
+++ src/main/java/com/example/demo/DatabaseConfig.java
@@ -2,22 +2,22 @@ package com.example.demo;
 
 import javax.sql.DataSource;
 
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
 public class DatabaseConfig {
 
     @Bean
     public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://mysql-0.mysql:3306/mysql");
-        dataSource.setUsername("root");
-        dataSource.setPassword("yourpassword");
-        return dataSource;
+         return DataSourceBuilder.create()
+          .driverClassName("com.mysql.cj.jdbc.Driver")
+          .url("jdbc:mysql://mysql-0.mysql:3306/mysql")
+          .username("root")
+          .password("yourpassword")
+          .build();
     }
 
     @Bean
