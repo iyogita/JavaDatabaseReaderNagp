0cc6ff041388c103e6a11916454a2a8e454d4e72
Yogita Singh <yogita.singh@nagarro.com>
2024-05-24 16:55:15 +0530

    new

--- src/main/java/com/example/demo/RecordController.java
+++ src/main/java/com/example/demo/RecordController.java
@@ -1,10 +1,10 @@
 package com.example.demo;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 public class RecordController {
--- src/main/java/com/example/demo/RecordRepository.java
+++ src/main/java/com/example/demo/RecordRepository.java
@@ -1,6 +1,17 @@
 package com.example.demo;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
-public interface RecordRepository extends JpaRepository<Record, Long> {
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+public class RecordRepository {
+    @PersistenceContext
+    private EntityManager em;
+
+    public List<Record> findAll() {
+        TypedQuery<Record> query = em.createQuery("SELECT r FROM Record r", Record.class);
+        return query.getResultList();
+    }
 }
